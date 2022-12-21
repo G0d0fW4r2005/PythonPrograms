63e5d131fc129dbb790277a38586449011a853b0
G0d0fW4r2005 <121112218+G0d0fW4r2005@users.noreply.github.com>
2022-12-21 17:04:12 +0100

    Create Par-Impar.py

---
+++ Par-Impar.py
@@ -0,0 +1,17 @@
+print("\n[\033[1;34m~\033[1;39m] Par-Impar by -H3llo2005-\n")
+def main():
+    try:
+        TN = int(input("\n[\033[1;34m+\033[1;39m] Dime un número entero: "))
+    except ValueError:
+        print("Inteligente un número entero no puede tener decimales y si has escrito letras pues es no funciona:(")
+        input("Enter para terminar: ")
+
+    if TN % 2 == 0:
+        print("El número es par")
+        input("Enter para terminar: ")
+    elif TN % 2 != 0:
+        print("El número es impar")
+        input("Enter para terminar: ")
+
+if __name__ == '__main__':
+    main()
